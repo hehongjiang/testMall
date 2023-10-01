4a04a633e937bb8dd9500584a3e05668aec156cc
hehongjiang <592449100@qq.com>
2023-10-01 23:16:24 +0800

    test

---
+++ src/views/home/childComps/test1.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <span>test1</span>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "test1"
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/home/childComps/test2.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <span>test2</span>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "test2"
+  }
+</script>
+
+<style scoped>
+
+</style>
