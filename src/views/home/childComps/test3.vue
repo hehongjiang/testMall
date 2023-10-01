3147673412524c7029cb8585d38bcb87e1cde751
hehongjiang <592449100@qq.com>
2023-10-01 23:25:37 +0800

    test

---
+++ src/views/home/childComps/test3.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="feature">
+    <span>test3</span>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "test3"
+  }
+</script>
+
+<style scoped>
+  .feature img {
+    width: 100%;
+  }
+</style>
