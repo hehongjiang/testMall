46bfe6186e074b1f105a4b8afc7c45f54faac29b
hehongjiang <592449100@qq.com>
2025-03-10 14:44:01 +0800

    dev-test11

---
+++ src/views/home/childComps/tset11.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+      <span>test11</span>
+    </div>
+  </template>
+  
+  <script>
+    export default {
+      name: "test11"
+    }
+    // dev
+  </script>
+  
+  <style scoped>
+  
+  </style>
+  
