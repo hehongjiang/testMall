24d57e542978b275ec26d041f101840efa8bcd25
hehongjiang <592449100@qq.com>
2025-03-10 09:35:21 +0800

    test4

---
+++ src/views/home/childComps/test4.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+      <span>test2</span>
+    </div>
+  </template>
+  
+  <script>
+    export default {
+      name: "test2"
+    }
+  </script>
+  
+  <style scoped>
+  
+  </style>
+  
