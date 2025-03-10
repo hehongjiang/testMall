3a7760799eb42e5675a26b135f559d8bdef90e8c
hehongjiang <592449100@qq.com>
2025-03-10 14:33:20 +0800

    test8

---
+++ src/views/home/childComps/test8.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+      <span>test8</span>
+    </div>
+  </template>
+  
+  <script>
+    export default {
+      name: "test8"
+    }
+    // test
+  </script>
+  
+  <style scoped>
+  
+  </style>
+  
