58f4b5d687a51f05b417e0698131239f342e619c
hehongjiang <592449100@qq.com>
2025-03-10 14:46:37 +0800

    dev-test13

---
+++ src/views/home/childComps/test13.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+      <span>test13</span>
+    </div>
+  </template>
+  
+  <script>
+    export default {
+      name: "test13"
+    }
+    // test
+  </script>
+  
+  <style scoped>
+  
+  </style>
+  
