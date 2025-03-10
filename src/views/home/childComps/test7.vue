a3fd999b349e1d3d5e6b0f452454d9288e1c34eb
hehongjiang <592449100@qq.com>
2025-03-10 14:19:32 +0800

    dev-test7

---
+++ src/views/home/childComps/test7.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+      <span>test7</span>
+    </div>
+  </template>
+  
+  <script>
+    export default {
+      name: "test7"
+    }
+  </script>
+  
+  <style scoped>
+  
+  </style>
+  
